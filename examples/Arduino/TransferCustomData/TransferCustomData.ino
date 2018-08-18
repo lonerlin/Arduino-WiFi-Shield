4cfffce71f33dfd95a5915b6561de796c6b8a99a
vsnetlin <vsnetlin@163.com>
2018-08-18 18:20:26 +0800

    next examples

---
+++ examples/Arduino/TransferCustomData/TransferCustomData.ino
@@ -0,0 +1,38 @@
+#include <dht11.h>
+#include <WiFiSerial.h>
+
+WiFiSerial wSerial(9600);
+dht11 DHT11;
+#define DHT11PIN 2
+
+void MessageEvent(String order,int paraOne,int paraTwo)
+{
+    digitalWrite(5,1);
+    if(order=="Temp")
+    {
+       int chk = DHT11.read(DHT11PIN);
+      wSerial.sendMessage("Temp",0,DHT11.temperature);
+    }
+    if(order=="Hum")
+    {
+       int chk = DHT11.read(DHT11PIN);
+      wSerial.sendMessage("Hum",0, DHT11.humidity); 
+    }  
+}
+
+void setup()
+{
+  Serial.begin(9600);
+ 
+  wSerial.Begin();
+  wSerial.setMessageEvent(MessageEvent);
+}
+
+void loop()
+{
+   wSerial.SerialEvent();
+  //Serial.print(dht11_pin_2.getTemperature());
+  //Serial.println();
+}
+
+
