ac4086c9c2cccc5c58e5e678e830882cfe92ea6f
vsnetlin <vsnetlin@163.com>
2018-08-27 19:42:59 +0800

    更新了4个默认命令的名称，避免跟作者自定义的重复。

--- examples/Python/Arduino.py
+++ examples/Python/Arduino.py
@@ -6,14 +6,14 @@ class Arduino:
         self.port=port
         self.arduino=ClientSocket(ip,port,single_use=False)
     def digitalWrite(self,pin,value):
-        return self.arduino.send("H,dw, " + str(pin) + ","+ str(value) + ", ")
+        return self.arduino.send("H,&dw, " + str(pin) + ","+ str(value) + ", ")
 
     def digitalRead(self,pin):
-        return self.arduino.send("H,dr," + str(pin) + ",0,").decode('utf-8').split(',')[3]
+        return self.arduino.send("H,&dr," + str(pin) + ",0,").decode('utf-8').split(',')[3]
     def analogWrite(self,pin,value):
-       return self.arduino.send("H,aw, " + str(pin) + "," + str(value) + ", ")
+       return self.arduino.send("H,&aw, " + str(pin) + "," + str(value) + ", ")
     def analogRead(self,pin):
-        value= self.arduino.send("H,ar," + str(pin) + ",0,").decode('utf-8').split(',')
+        value= self.arduino.send("H,&ar," + str(pin) + ",0,").decode('utf-8').split(',')
         return  value[3]
 
     def sendMessage(self,Order,ParaOne,ParaTwo):
