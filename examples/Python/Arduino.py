5c1a726d820c563cf6036890910919516a68a66f
vsnetlin <vsnetlin@163.com>
2018-09-05 15:45:32 +0800

    update digitalWrite funciton.

--- examples/Python/Arduino.py
+++ examples/Python/Arduino.py
@@ -10,14 +10,14 @@ class Arduino:
         self.arduino=ClientSocket(ip,port,single_use=False)
 
     def digitalWrite(self,pin,value):
-        return self.arduino.send("H,$dw, " + str(pin) + ","+ str(value) + ", ")
-
+        self.arduino.send("H,$dw, " + str(pin) + ","+ str(value) + ", ")
+        return None
     def digitalRead(self,pin):
         return self.arduino.send("H,$dr," + str(pin) + ",0,").decode('utf-8').split(',')[3]
 
     def analogWrite(self,pin,value):
-       return self.arduino.send("H,$aw, " + str(pin) + "," + str(value) + ", ")
-
+       self.arduino.send("H,$aw, " + str(pin) + "," + str(value) + ", ")
+       return None
     def analogRead(self,pin):
         value= self.arduino.send("H,$ar," + str(pin) + ",0,").decode('utf-8').split(',')
         return  value[3]
